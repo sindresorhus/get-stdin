111d7cb19307fc3fea109bef99404fc0ac9124c4
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-17 22:03:34 +0200

    Test programmatically

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 'use strict';
+var equal = require('buffer-equal');
 var test = require('ava');
 var stdin = require('./');
 
@@ -8,4 +9,19 @@ test('get stdin', function (t) {
 	stdin(function (data) {
 		t.assert(data.trim() === 'unicorns');
 	});
+
+	process.stdin.emit('data', 'unicorns');
+	process.stdin.emit('end');
+});
+
+test('get stdin as a buffer', function (t) {
+	t.plan(2);
+
+	stdin.buffer(function (data) {
+		t.assert(equal(data, new Buffer('unicorns')));
+		t.assert(data.toString().trim() === 'unicorns');
+	});
+
+	process.stdin.emit('data', new Buffer('unicorns'));
+	process.stdin.emit('end');
 });
