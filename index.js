0ec4bd297f3e71797bd9e91407f6012bf53d32f8
Sindre Sorhus <sindresorhus@gmail.com>
2014-08-17 20:59:35 +0200

    improve perf

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 module.exports = function (cb) {
 	var ret = [];
+	var len = 0;
 
-	process.stdin.on('data', function (data) {
-		ret.push(data);
+	process.stdin.on('data', function (chunk) {
+		ret.push(chunk);
+		len += chunk.length;
 	});
 
 	process.stdin.on('end', function () {
-		cb(Buffer.concat(ret));
+		cb(Buffer.concat(ret, len));
 	});
 };
