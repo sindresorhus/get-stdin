d2c098e089e64ba5b9fc4df8c68b8dc6848d8d89
Sindre Sorhus <sindresorhus@gmail.com>
2014-11-23 15:17:17 +0700

    return empty string/buffer when `process.stdin.isTTY === false`
    fixes #1

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict';
-var equal = require('buffer-equal');
 var test = require('ava');
+var bufferEqual = require('buffer-equal');
 var stdin = require('./');
 
 test('get stdin', function (t) {
@@ -18,10 +18,36 @@ test('get stdin as a buffer', function (t) {
 	t.plan(2);
 
 	stdin.buffer(function (data) {
-		t.assert(equal(data, new Buffer('unicorns')));
+		t.assert(bufferEqual(data, new Buffer('unicorns')));
 		t.assert(data.toString().trim() === 'unicorns');
 	});
 
 	process.stdin.emit('data', new Buffer('unicorns'));
 	process.stdin.emit('end');
+});
+
+test('get empty string when no stdin', function (t) {
+	var _ = process.stdin.isTTY;
+	process.stdin.isTTY = false;
+
+	t.plan(1);
+
+	stdin(function (data) {
+		t.assert(data === '');
+	});
+
+	process.stdin.isTTY = _;
+});
+
+test('get empty buffer when no stdin', function (t) {
+	var _ = process.stdin.isTTY;
+	process.stdin.isTTY = false;
+
+	t.plan(1);
+
+	stdin.buffer(function (data) {
+		t.assert(bufferEqual(data, new Buffer('')));
+	});
+
+	process.stdin.isTTY = _;
 });
